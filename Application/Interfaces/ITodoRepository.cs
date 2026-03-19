9919672b4d7430c637fd109286058bb1301daa88
Mavis Chan <mavischan@Maviss-MacBook-Air.local>
2026-03-19 12:26:16 +0700

    feat: define repository interface

---
+++ Application/Interfaces/ITodoRepository.cs
@@ -0,0 +1,11 @@
+using tiktik.Domain.Entities;
+namespace tiktik.Application.Interfaces;
+
+public interface ITodoRepository
+{
+    Task<List<TodoTask>> GetByUserIdAsync(int userId);
+    Task<TodoTask?> GetByIdAsync(int taskId);
+    Task AddAsync(TodoTask task);
+    Task UpdateAsync(TodoTask task);
+    Task DeleteAsync(int taskId);
+}
