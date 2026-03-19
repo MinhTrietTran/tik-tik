e267b94ac6d0ffc5b7892946fdeead7f31e48964
Mavis Chan <mavischan@Maviss-MacBook-Air.local>
2026-03-19 13:15:19 +0700

    feat: implement todo service with core use cases

---
+++ Application/Services/TodoService.cs
@@ -0,0 +1,52 @@
+using tiktik.Domain.Entities;
+using tiktik.Application.Interfaces;
+
+namespace tiktik.Application.Services;
+
+public class TodoService
+{
+    private readonly ITodoRepository _repo;
+    public TodoService(ITodoRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task<List<TodoTask>> GetTasksByUserIdAsync(int userId)
+    {
+        return await _repo.GetByUserIdAsync(userId);
+    }
+
+    public async Task CreateAsync(string title, int userId)
+    {
+        //1. Validate input
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Title cannot be empty.");
+        
+        //2. Create entity
+        var task = new TodoTask
+        {
+            Title = title,
+            CreateAt = DateTime.UtcNow,
+            IsDone = false,
+            UserId = userId
+        };
+
+        //3. Call repository
+        await _repo.AddAsync(task);
+    }
+
+    public async Task MarkAsDoneAsync(int taskId)
+    {
+        //1. Get task
+        var task = await _repo.GetByIdAsync(taskId);
+        //2. if null throw handle exception
+        if (task == null)
+            throw new KeyNotFoundException("Task not found.");
+        //3. Update IsDone
+        task.IsDone = true;
+
+
+        //3. Call repository
+        await _repo.UpdateAsync(task);
+    }
+}
--- obj/Debug/net8.0/tiktik.AssemblyInfo.cs
+++ obj/Debug/net8.0/tiktik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tiktik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9919672b4d7430c637fd109286058bb1301daa88")]
 [assembly: System.Reflection.AssemblyProductAttribute("tiktik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tiktik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
